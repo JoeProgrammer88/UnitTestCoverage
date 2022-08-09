b117bb048eecbb1f7fa861f047b0807a8a0d45b5
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2022-08-08 20:39:44 -0700

    Add project files.

---
+++ UnitTestCoverageTests/BasicMathTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestCoverage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestCoverage.Tests
+{
+    [TestClass()]
+    public class BasicMathTests
+    {
+        [TestMethod()]
+        public void Add_ValidIntegers_ReturnsSumOfTwoNumbers()
+        {
+            int result = BasicMath.Add(5, 3);
+            Assert.AreEqual(result, 8);
+        }
+    }
+}
